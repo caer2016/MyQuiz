154533e87dbe9c5d5e49de4032894b70f7c51259
Kai <vantienkhai.uet@gmail.com>
2021-05-04 10:05:51 +0700

    first commit

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request, 'pages/home.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def register(request):
+    return render(request, 'pages/register.html')
