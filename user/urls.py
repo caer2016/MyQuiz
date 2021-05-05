59e5a744faeedeedc6f07bad2d81b509ed1ae5fb
0thElement <quangquangtrung89@gmail.com>
2021-05-05 16:29:11 +0700

    UI for register/unregistering quizpacks + learning

--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,9 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('student_dashboard/', views.student_dashboard),
-    path('teacher_dashboard/', views.teacher_dashboard),
+    path('student_dashboard/', views.student_dashboard, name = 'student_dashboard'),
+    path('teacher_dashboard/', views.teacher_dashboard, name = 'teacher_dashboard'),
+    path('pack/', views.unregistered_quizpack_list_view, name = 'all_quizpack'),
+    path('pack/add/<int:id>', views.register_quizpack_view, name = 'register_pack'),
+    path('pack/remove/<int:id>/', views.unregister_quizpack_view, name = 'unregister_pack'),
 ]
