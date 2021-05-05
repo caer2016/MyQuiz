02e856c8a539570df52f0e8ac97934f65183d56a
lamcherry0208 <lamcherry0208@gmail.com>
2021-05-05 18:36:06 +0700

    new

--- user/urls.py
+++ user/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('user/student_dashboard/', views.student_dashboard),
     path('user/teacher_dashboard/', views.teacher_dashboard),
+    path('user/teacher_dashboard/courses/new_course', views.create_new_course),
     path('courses_list/', views.show_courses_list, name='courses'),
     path('courses_list/<int:id>/', views.course_data, name='course_data'),
 ]
--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . import forms
 from .models import QUser, Courses
+from home.views import is_teacher, is_student
 
 # Create your views here.
 
@@ -21,3 +22,12 @@ def show_courses_list(request):
 def course_data(request, id):
     courseInfo = Courses.objects.get(id=id)
     return render(request, 'course_pages/course_info.html', {'course': courseInfo})
+
+def create_new_course(request):
+    if is_teacher(request.user):
+        newCourseName = request.POST['newCourseName']
+        newManageTeacher = request.user
+        curC = Courses.objects.create(courseName = newCourseName, manageTeacher = newManageTeacher)
+        curC.save()
+
+        return render(request, "course_pages/new_course.html")
